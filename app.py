e283888969edfa8292bd9c7089af6e548b99adad
Tuana Celik <tuana.celik@deepset.ai>
2023-02-23 20:55:01 +0100

    adding sidebar for open ai key

--- app.py
+++ app.py
@@ -3,48 +3,57 @@ from annotated_text import annotation
 from json import JSONDecodeError
 import logging
 from markdown import markdown
+import requests
 
 import streamlit as st
 
-from utils.haystack import query
-from utils.ui import reset_results, set_initial_state
+from utils.haystack import start_haystack, query
+from utils.ui import reset_results, set_initial_state, sidebar
+from utils.config import TWITTER_BEARER
 
 set_initial_state()
 
 st.write("# 🐤 What have they been tweeting about lately?")
 
-search_bar, button = st.columns(2)
-# Search bar
-with search_bar: 
-    username = st.text_input("Please provide a twitter username", on_change=reset_results)
+sidebar()
 
-with button: 
-    st.write("")
-    st.write("")
-    run_pressed = st.button("Search tweets")
 
-run_query = (
-    run_pressed or username != st.session_state.username
-)
+if st.session_state.get("OPENAI_API_KEY"):
+    prompter, template = start_haystack(st.session_state.get("OPENAI_API_KEY"))
+    st.session_state["api_key_configured"] = True
+    search_bar, button = st.columns(2)
+    # Search bar
+    with search_bar: 
+        username = st.text_input("Please provide a twitter username", on_change=reset_results)
 
-# Get results for query
-if run_query and username:
-    reset_results()
-    st.session_state.username = username
-    with st.spinner("🔎"):
-        try:
-            st.session_state.result = query(username)
-        except JSONDecodeError as je:
-            st.error(
-                "👓 &nbsp;&nbsp; An error occurred reading the results. Is the document store working?"
-            )    
-        except Exception as e:
-            logging.exception(e)
-            st.error("🐞 &nbsp;&nbsp; An error occurred during the request.")
-        
+    with button: 
+        st.write("")
+        st.write("")
+        run_pressed = st.button("Search tweets")
+else:
+    st.write("Please provide your OpenAI Key to start using the application")
+    
+if st.session_state.get("api_key_configured"):
+    run_query = (
+        run_pressed or username != st.session_state.username
+    )
+
+    # Get results for query
+    if run_query and username:
+        reset_results()
+        st.session_state.username = username
+        with st.spinner("🔎"):
+            try:
+                st.session_state.result = query(username, prompter, template)
+            except JSONDecodeError as je:
+                st.error(
+                    "👓 &nbsp;&nbsp; An error occurred reading the results. Is the document store working?"
+                )    
+            except Exception as e:
+                logging.exception(e)
+                st.error("🐞 &nbsp;&nbsp; An error occurred during the request.")            
+                
+    if st.session_state.result:
+        voice = st.session_state.result
+        st.write(voice[0])
             
-
-if st.session_state.result:
-    voice = st.session_state.result
-    st.write(voice[0])
-        
--- utils/ui.py
+++ utils/ui.py
@@ -7,6 +7,41 @@ def set_state_if_absent(key, value):
 def set_initial_state():
     set_state_if_absent("username", "Provide a Twitter username")
     set_state_if_absent("result", None)
+    set_state_if_absent("haystack_started", False)
 
 def reset_results(*args):
     st.session_state.result = None
+
+def set_openai_api_key(api_key: str):
+    st.session_state["OPENAI_API_KEY"] = api_key
+
+def sidebar():
+    with st.sidebar:
+        st.markdown(
+            "## How to use\n"
+            "1. Enter your [OpenAI API key](https://platform.openai.com/account/api-keys) below🔑\n"
+            "2. Enter a twitter username in the searchbar\n"
+            "3. Enjoy 🤗\n"
+        )
+        api_key_input = st.text_input(
+            "OpenAI API Key",
+            type="password",
+            placeholder="Paste your OpenAI API key here (sk-...)",
+            help="You can get your API key from https://platform.openai.com/account/api-keys.",
+            value=st.session_state.get("OPENAI_API_KEY", ""),
+        )
+
+        if api_key_input:
+            set_openai_api_key(api_key_input)
+
+        st.markdown("---")
+        st.markdown("### About\n"
+                    "This app is just for fun and there are many points of improvement."
+                    "It was built with [Haystack](https://haystack.deepset.ai) using the"
+                    " [PromptNode](https://docs.haystack.deepset.ai/docs/prompt_node) and custom [PromptTemplate](https://docs.haystack.deepset.ai/docs/prompt_node#templates)"
+                    "The source code is also on [GitHub](https://github.com/TuanaCelik/should-i-follow)"
+                    "with instructions to run locally.")
+        st.markdown("Made by [tuanacelik](https://twitter.com/tuanacelik)")
+        st.markdown("---")
+        st.markdown("""Thanks to [mmz_001](https://twitter.com/mm_sasmitha) 
+                        for open sourcing [KnowledgeGPT](https://knowledgegpt.streamlit.app/) which helped me with this sidebar 🙏🏽""")
